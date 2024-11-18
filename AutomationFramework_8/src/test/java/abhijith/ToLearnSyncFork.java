4ddcd4fddbaeebcaf3fbd3cd894410d37efc2ace
AbhijithTyss <abhijith.styss@gmail.com>
2024-11-18 08:58:38 +0530

    Sync fork class added

---
+++ AutomationFramework_8/src/test/java/abhijith/ToLearnSyncFork.java
@@ -0,0 +1,11 @@
+package abhijith;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ToLearnSyncFork {
+	@Test
+	public void sync() {
+		Reporter.log("Sync fork class added");
+	}
+}
